3f1b657db6879c6fa32aa2b164a1251174cbb795
Simon Tidman <simontidman@Simons-MBP-2.lan>
2021-06-04 21:43:28 +0100

    added captcha

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,10 +1,10 @@
 
-
     $(window).ready(function() {
 
       // Preloader
-      $('.loader').hide();
-      $('.loader-mask').delay(350).hide('slow');
+      $('.loader-mask').delay(350).toggle('slow');
+      $('.loader').fadeOut();
+    
 
  
 
@@ -12,24 +12,40 @@
 
 
 
-
 $(document).ready(function() {
 
+//   /* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
+// var prevScrollpos = window.pageYOffset;
+// window.onscroll = function() {
+//   var currentScrollPos = window.pageYOffset;
+//   if (prevScrollpos > currentScrollPos) {
+//     document.getElementById("video").style.display = "block";
+//   } else {
+//     document.getElementById("video").hide().fadeOut('slow');
+//   }
+//   prevScrollpos = currentScrollPos;
+// }
 
-//hide landing page image onscrfeen
 
-    $(window).scroll(function(e) {
-        e.preventDefault();
-        if ($(this).scrollTop() > 0) {
-          $('.a').hide().fadeOut('slow');
-        }
-        else {
-          $('.a').show();
-        }
-      });
+  //hide landing page image onscrfeen
+  
+      $(window).scroll(function(e) {
+          e.preventDefault();
+          if ($(this).scrollTop() > 0) {
+            $('#video').hide().fadeOut('slow');
+          }
+          else {
+            $('#video').show();
+          }
+        });
+  
+        
+  
+  });
+  
 
-      
 
-});
+
+
 
 
